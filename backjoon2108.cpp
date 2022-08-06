85555bbbd7a65d5de4574e03b524f290cf4ccf4d
jaejune Yun <jjune0427@naver.com>
2022-08-07 01:46:52 +0900

    ȯ°æ ¼³Á¤

---
+++ backjoon2108.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include<algorithm>
+#include <math.h>
+
+
+using namespace std;
+
+//n = 홀수
+//산술평균: n개의 수들의 합을 n으로 나눈 값
+//중앙값: n개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
+//최빈값: N개의 수들 중 가장 많이 나타나는 값
+//범위: n개의 수들 중 최댓값과 최솟값의 차이
+
+//입력: n개의 입력을 받음
+//첫줄 산술평균(첫째자리에서 반올림), 둘째줄 중앙값, 셋째 최빈값(최빈값중 두번째로 작은 수), 넷째 범위
+
+int nums[500000];
+int appear[500000];
+
+int main()
+{
+	cout << "안녕";
+	// int n; double sum = 0;
+	// cin >> n;
+	// for (int i = 0; i < n; i++) {
+	// 	cin >> nums[i];
+	// 	//더하기
+	// 	sum += nums[i];
+	// }
+
+	// cout << round(sum / n);
+
+	// stable_sort(nums, nums+n);
+
+	//int start=nums, end=n;
+}
