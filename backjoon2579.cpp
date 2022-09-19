a654b094049d48b1679d35dc077262ad38b460ba
jaejune Yun <jjune0427@naver.com>
2022-09-19 23:37:16 +0900

    백준 2579번 성공

--- backjoon2579.cpp
+++ backjoon2579.cpp
@@ -10,16 +10,19 @@ int main()
     int n;      //계단의 갯수
     cin >> n;       ///계단 갯수 입력
 
-    int stairs[301];
-    int dp[301];
+    int stairs[302];
+    int dp[302];
 
-    for(int i=0; i<n; i++){
+    for(int i=1; i<=n; i++){
         cin >> stairs[i];
     }
 
-    dp[0] = stairs[0];
-    dp[1] = max(stairs[1], stairs[0] + stairs[1]);
-    dp[2] = 
-    
-      
+    dp[0] = 0;
+    dp[1] = stairs[1];                  //1번 계단의 최대값
+    dp[2] = stairs[1] + stairs[2];      //2번 계단의 최대값
+    for(int i = 3; i<=n; i++){
+        dp[i] = max(dp[i-2] + stairs[i], dp[i-3]+stairs[i-1]+stairs[i]);
+    }
+
+    cout << dp[n];
 }
