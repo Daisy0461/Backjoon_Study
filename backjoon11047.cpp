4f4631c41229ac68a7178da70ef268cc5750b4de
jaejune Yun <jjune0427@naver.com>
2022-09-20 00:03:21 +0900

    11047 주석 제거

--- backjoon11047.cpp
+++ backjoon11047.cpp
@@ -1,31 +1,3 @@
-// #include <iostream>
-
-// using namespace std;
-
-// int main(){
-//     ios_base::sync_with_stdio(0);
-//     cin.tie(0);
-
-//     int n, k;
-//     cin >> n >> k;      //n과 k입력 n은 동전의 종류, k라는 값을 만들어야함.
-//     int count=0;
-
-//     int money[11] = {0, };
-
-//     for(int i=0; i<n; i++){
-//         cin >> money[i];        //0~n-1까지 입력받음.
-//     }
-
-
-//     for(int i = n-1; i>=0; i--){
-//         count += k/money[i];
-//         k = k%money[i];
-//     }
-    
-//     cout << count << "\n";
-
-// }
-
 #include <iostream>
 
 using namespace std;
