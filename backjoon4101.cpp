24ac4c7077bf77534d3d04f74d88349689d5f58f
jaejune Yun <jjune0427@naver.com>
2022-09-30 22:18:16 +0900

    Create backjoon4101.cpp

---
+++ backjoon4101.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int a=1, b=1;
+    cout << "12341234";
+    
+    while(a != 0 && b != 0){
+        cin >> a, b;
+        cout << "asdasd";
+        if(a > b){
+            cout << "Yes" << "\n";
+        }else{
+            cout << "No" << "\n";
+        }
+    }
+}
